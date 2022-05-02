b9234b709a014be20e23926e2d5b1283ecb7c62e
Kacper Woźniak <kacper.wozniak@billennium.com>
2022-05-02 15:19:29 +0200

    clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,44 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import React from 'react'
 import fs from 'fs'
 import matter from 'gray-matter'
 import styles from '../styles/Home.module.css'
 
-type PROPS = {posts: {
-  slug: string;
-  title: string;
-}[]}
+type PROPS = {
+  posts: {
+    slug: string;
+    title: string;
+  }[]
+}
 
-const Home: NextPage<PROPS> = (props) => {
-
+const Home: NextPage<PROPS> = (props: PROPS) => {
   return (
     <div className={styles.container}>
       <h1>Blog</h1>
-      {props.posts.map((item, index)=>{
-        return <a href={`/post/${item.slug}`} key={index}>
-          <div  className={styles.card} >
-          <p>{item.title}</p>
+      {props.posts.map((post, index) => {
+        return <Link href={`/post/${post.slug}`} passHref key={index}>
+          <div className={styles.card} >
+            <p>{post.title}</p>
           </div>
-        </a>
+        </Link>
       })}
     </div>
   )
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync('posts');
+  const files = fs.readdirSync('posts')
 
   const posts = files.map((fileName) => {
     const slug = fileName.replace('.md', '')
     const read = fs.readFileSync(`posts/${fileName}`, 'utf-8')
-    const { data: frontmatter } = matter(read);
+    const { data: frontmatter } = matter(read)
 
     return {
       slug,
-      title: frontmatter.title,
-    };
+      title: frontmatter.title
+    }
   })
 
   return {
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import fs from 'fs'
 import matter from 'gray-matter'
 import styles from '../../styles/Home.module.css'
@@ -8,11 +9,11 @@ type PROPS = {
   content: string;
 }
 
-const Slug: NextPage<PROPS> = (props) => {
+const Slug: NextPage<PROPS> = (props: PROPS) => {
 
   return (
     <div className={styles.container}>
-      <a href="/" >Back</a>
+      <Link href="/" >Back</Link>
       <h1>{props.title}</h1>
       {props.content}
     </div>
@@ -20,34 +21,34 @@ const Slug: NextPage<PROPS> = (props) => {
 }
 
 type PARAMS = {
-    params: {
-        slug: string;
-    }
+  params: {
+    slug: string;
+  }
 }
 
 export async function getStaticPaths() {
-    const files = fs.readdirSync('posts');
-    const paths = files.map((fileName) => ({
-        params: {
-          slug: fileName.replace('.md', ''),
-        },
-    }));
+  const files = fs.readdirSync('posts');
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace('.md', ''),
+    },
+  }));
 
-    return {
-        paths,
-        fallback: false,
-    };
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({ params: { slug } }:  PARAMS) {
-    const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
-    const { data: frontmatter, content } = matter(fileName);
-    return {
-      props: {
-        title: frontmatter.title,
-        content,
-      },
-    };
-  }
+export async function getStaticProps({ params: { slug } }: PARAMS) {
+  const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
+  const { data: frontmatter, content } = matter(fileName);
+  return {
+    props: {
+      title: frontmatter.title,
+      content,
+    },
+  };
+}
 
 export default Slug
